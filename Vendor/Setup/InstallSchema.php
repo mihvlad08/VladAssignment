0d2bfcc1d22bb5e81e00669442ada43fb33408d2
mihvlad08 <mihvlad08@gmail.com>
2022-03-24 14:33:57 +0200

    first 3 tasks

---
+++ Vendor/Setup/InstallSchema.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace VladAssignment\Vendor\Setup;
+
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+        if (!$installer->tableExists('assignment_vendor_entity')) {
+            $table = $installer->getConnection()->newTable(
+                $installer->getTable('assignment_vendor_entity')
+            )
+                ->addColumn(
+                    'vendor_id',
+                    Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => true,
+                        'nullable' => false,
+                        'primary'  => true,
+                        'unsigned' => true,
+                    ],
+                    'ID'
+                )
+                ->addColumn(
+                    'name',
+                    Table::TYPE_TEXT,
+                    255,
+                    ['nullable => false'],
+                    'Post Name'
+                )
+                ->addColumn(
+                    'status',
+                    Table::TYPE_INTEGER,
+                    1,
+                    [],
+                    'Status'
+                )
+                ->addColumn(
+                    'email',
+                    Table::TYPE_TEXT,
+                    255,
+                    ['nullable => false'],
+                    'email'
+                )
+                ->setComment('Vendors Table');
+            $installer->getConnection()->createTable($table);
+
+            $installer->getConnection()->addIndex(
+                $installer->getTable('assignment_vendor_entity'),
+                $setup->getIdxName(
+                    $installer->getTable('assignment_vendor_entity'),
+                    ['name', 'email'],
+                    AdapterInterface::INDEX_TYPE_FULLTEXT
+                ),
+                ['name', 'email'],
+                AdapterInterface::INDEX_TYPE_FULLTEXT
+            );
+        }
+        $installer->endSetup();
+    }
+}
---
+++ Vendor/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'VladAssignment_Vendor',
+    __DIR__
+);
